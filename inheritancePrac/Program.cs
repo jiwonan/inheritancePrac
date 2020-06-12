8c69bea2e110ffe6c475a2c33577ef0342b9ccd8
Jiwon An <s2018s31@e-mirim.hs.kr>
2020-06-12 13:20:09 +0900

    Base를 이용한 상속

--- inheritancePrac/Program.cs
+++ inheritancePrac/Program.cs
@@ -10,25 +10,27 @@ namespace inheritancePrac
     {
         static void Main(string[] args)
         {
-            List<Animal> animals =
-                new List<Animal>
-                {
-                    new Dog(), new Cat(), new Cat(),
-                    new Dog(), new Dog(), new Cat()
-                };
+            //List<Animal> animals =
+            //    new List<Animal>
+            //    {
+            //        new Dog(), new Cat(), new Cat(),
+            //        new Dog(), new Dog(), new Cat()
+            //    };
 
-            foreach(var item in animals)
-            {
+            //foreach(var item in animals)
+            //{
 
-                item.Eat();
-                item.Sleep();
+            //    item.Eat();
+            //    item.Sleep();
 
-                var dog = item as Dog;
-                if (dog != null) { dog.Bark(); }
+            //    var dog = item as Dog;
+            //    if (dog != null) { dog.Bark(); }
 
-                var cat = item as Cat;
-                if (cat != null) { cat.Meow(); }
-            }
+            //    var cat = item as Cat;
+            //    if (cat != null) { cat.Meow(); }
+            //}
+
+            Child c = new Child(11);
         }
     }
 }
