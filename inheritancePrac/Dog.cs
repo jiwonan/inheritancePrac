72ec8f84ac399b58c534aa1b89c574a9a5bdb998
Jiwon An <s2018s31@e-mirim.hs.kr>
2020-06-12 11:30:24 +0900

    초기 커밋.

---
+++ inheritancePrac/Dog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritancePrac
+{
+    class Dog : Animal
+    {
+        public int Age { get; set; }
+        public string Color { get; set; }
+
+        public Dog() { Color = "하얀색"; }
+        public void Bark() { Console.WriteLine("왕왕~!!"); }
+
+    }
+}
---
+++ inheritancePrac/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritancePrac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals =
+                new List<Animal>
+                {
+                    new Dog(), new Cat(), new Cat(),
+                    new Dog(), new Dog(), new Cat()
+                };
+
+            foreach(var item in animals)
+            {
+                item.Eat();
+                item.Sleep();
+            }
+
+            /*foreach(Dog dog in dogs)
+            {
+                dog.Eat();
+                dog.Bark();
+                dog.Sleep();
+            }
+            foreach (Dog dog in dogs)
+            {
+                dog.Eat();
+                dog.Bark();
+                dog.Sleep();
+            }*/
+        }
+    }
+}
